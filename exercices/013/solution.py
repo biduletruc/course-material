7078f62a10d251a576fa768ab41826b06b21fa29
Marie Yeme <mrs.eriamy@gmail.com>
2014-09-23 12:04:17 +0200

    solution1

---
+++ exercices/013/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 16:58:16 2014
+
+@author: Tortank
+"""
+import sys
+if len(sys.argv) == 1:
+    print("usage: python3 solution.py PARAM")
+else:
+    print sys.argv[1]
---
+++ exercices/014/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 11:30:25 2014
+
+@author: Tortank
+"""
+
+print(245850922 / 78256779)
+print((1 + (5**0.5)) / 2)
